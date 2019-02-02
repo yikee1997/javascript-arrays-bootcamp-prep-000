9fd13f68c4729e96021e1da36c6626fb479d4e8b
YI KEE <marioxd1997@gmail.com>
2019-02-02 05:58:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,8 +14,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
-  return array;
+  return array.push(element);
 }
 
 function accessElementInArray(array, index) {
