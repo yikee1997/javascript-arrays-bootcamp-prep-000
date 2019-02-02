27f4f9b2e61d447117b4d305c09008570c8fde0c
YI KEE <marioxd1997@gmail.com>
2019-02-02 01:28:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,14 @@
 var chocolateBars = ["snickers, hundred grand, kitkat, skittles"]
+
+function addElementToBeginningOfArray() {
+  return [..., chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray() {
+  chocolateBars.unshift("candy");
+}
+function addElementToEndOfArray() {
+  return [chocolateBars, ...]
+}
+function destructivelyAddElementToEndOfArray() {
+  chocolateBars.push("candy")
+}
