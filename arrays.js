e6008aedb9b2d55d8fe9e6ed6c98a1c70b4e3999
YI KEE <marioxd1997@gmail.com>
2019-02-02 04:49:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(){
   var element = ["adsd"];
   addElementToBeginningOfArray(element, ...chocolateBars);
-  return addElementToBeginningOfArray();
 }
