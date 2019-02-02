c48b99534056e4d5b4c2e5bfbff03f3a5d84a05d
YI KEE <marioxd1997@gmail.com>
2019-02-02 05:57:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
@@ -38,6 +37,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0);
-  return array;
+  return array.slice(0, array.length - 1);
 }
