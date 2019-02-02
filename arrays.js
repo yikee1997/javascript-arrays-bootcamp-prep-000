7feefe8de3240f5401d9cc7d19e3e12989fb89ab
YI KEE <marioxd1997@gmail.com>
2019-02-02 04:36:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,41 +1,5 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-//var candyString = "foo"
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars, candyString){
-  return [candyString, ...chocolateBars];
+function addElementToBeginningOfArray([1], 'foo') {
+  addElementToBeginningOfArray = ['foo', 1]
 }
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
-  chocolateBars.unshift(candyString);
-  return chocolateBars;
-}
-
-function addElementToEndOfArray(chocolateBars, candyString){
-  return [...chocolateBars, candyString];
-}
-
-function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
-  chocolateBars.push(candyString);
-  return chocolateBars;
-}
-
-function accessElementInArray(array, index){
-  return array[index];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.shift();
-  return chocolateBars;
-}
-
-function removeElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.slice(1);
-}
-
-function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-  chocolateBars.pop();
-  return chocolateBars;
-}
-
-function removeElementFromEndOfArray(chocolateBars){
-  return choc
