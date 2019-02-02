e8797486007bb7ba58624c8c1e257d70fe881ad2
YI KEE <marioxd1997@gmail.com>
2019-02-02 05:42:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.push(element);
+  array.unshift(element);
   return array;
 }
 
