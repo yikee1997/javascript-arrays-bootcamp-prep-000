73f982c741d62fed7dd91da7fc74fa4b7204d6bc
YI KEE <marioxd1997@gmail.com>
2019-02-02 05:18:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
   return ['foo', 1];
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return ['foo', 1];
+}
