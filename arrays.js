83ade7cc44830d130e9a29feaa38d969965e4955
YI KEE <marioxd1997@gmail.com>
2019-02-02 05:49:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return [1, 'foo'];
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
@@ -23,5 +24,20 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1);
   return array;
 }
